8a7760b2b36d6ae85acc5e432be387c588f5183e
Skyelar Craver <skyelar.craver@cellink.com>
2024-02-08 19:43:10 -0500

    add ability to parse escaped characters, and to search line for match

--- print3rs-console/src/logging/parsing.rs
+++ print3rs-console/src/logging/parsing.rs
@@ -1,20 +1,23 @@
 use crate::commands::Command;
 use winnow::{
     ascii::{alphanumeric1, float, space1},
-    combinator::{alt, delimited, preceded, repeat},
+    combinator::{alt, delimited, dispatch, empty, fail, preceded, repeat, rest},
     prelude::*,
-    token::take_till,
+    stream::AsChar,
+    token::{take, take_till, take_until},
 };
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub enum Segment<'a> {
     Tag(&'a str),
+    Escaped(char),
     Value(&'a str),
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 enum OwnedSegment {
     Tag(String),
+    Escaped(char),
     Value,
 }
 
@@ -22,13 +25,23 @@ impl<'a> From<Segment<'a>> for OwnedSegment {
     fn from(value: Segment<'a>) -> Self {
         match value {
             Segment::Tag(s) => OwnedSegment::Tag(s.to_string()),
+            Segment::Escaped(c) => OwnedSegment::Escaped(c),
             Segment::Value(_) => OwnedSegment::Value,
         }
     }
 }
 
 fn parse_tag<'a>(input: &mut &'a str) -> PResult<Segment<'a>> {
-    Ok(Segment::Tag(take_till(1.., '{').parse_next(input)?))
+    Ok(Segment::Tag(take_till(1.., ('{', '}')).parse_next(input)?))
+}
+
+fn parse_escape<'a>(input: &mut &'a str) -> PResult<Segment<'a>> {
+    dispatch! {take(2usize);
+    "{{" => empty.map(|_| Segment::Escaped('{')),
+    "}}" => empty.map(|_| Segment::Escaped('}')),
+    _ => fail,
+    }
+    .parse_next(input)
 }
 
 fn parse_value<'a>(input: &mut &'a str) -> PResult<Segment<'a>> {
@@ -38,7 +51,7 @@ fn parse_value<'a>(input: &mut &'a str) -> PResult<Segment<'a>> {
 }
 
 fn parse_segment<'a>(input: &mut &'a str) -> PResult<Segment<'a>> {
-    alt((parse_tag, parse_value)).parse_next(input)
+    alt((parse_tag, parse_escape, parse_value)).parse_next(input)
 }
 
 pub fn parse_segments<'a>(input: &mut &'a str) -> PResult<Vec<Segment<'a>>> {
@@ -61,16 +74,40 @@ pub fn make_parser(segments: Vec<Segment<'_>>) -> impl FnMut(&mut &[u8]) -> PRes
         .collect::<Vec<_>>();
     move |input: &mut &[u8]| -> PResult<Vec<f32>> {
         let mut values = vec![];
+
+        // skips up to pattern start
+        if let Some(first) = segments.first() {
+            match first {
+                OwnedSegment::Tag(tag) => {
+                    take_until(0.., tag.as_bytes()).void().parse_next(input)?;
+                }
+                OwnedSegment::Escaped(c) => {
+                    take_till(0.., |i| (*c as u8) == i)
+                        .void()
+                        .parse_next(input)?;
+                }
+                OwnedSegment::Value => {
+                    take_till(0.., |i: u8| i.is_dec_digit() || [b'.', b'-'].contains(&i))
+                        .void()
+                        .parse_next(input)?;
+                }
+            };
+        }
         for segment in segments.iter() {
             match segment {
                 OwnedSegment::Tag(ref s) => {
                     s.as_bytes().parse_next(input)?;
+                }
+                OwnedSegment::Escaped(mut c) => {
+                    c.parse_next(input)?;
                 }
                 OwnedSegment::Value => {
                     values.push(float.parse_next(input)?);
                 }
             };
         }
+        // ignores rest of pattern
+        rest.parse_next(input)?;
         Ok(values)
     }
 }
@@ -83,6 +120,11 @@ pub fn get_headers(segments: &[Segment]) -> String {
             s.push(',');
         }
     }
+    // strip trailing
+    if s.ends_with(',') {
+        s.pop();
+    };
+    s.push('\n');
     s
 }
 
@@ -118,6 +160,33 @@ mod tests {
         let mut parser = make_parser(segments);
         let final_out = parser
             .parse(b"millis: 1234.5,pos:-4.0,current:100")
+            .unwrap();
+        assert_eq!(final_out, vec![1234.5, -4.0, 100.0]);
+    }
+
+    #[test]
+    fn test_escaped_braces() {
+        let parse_pattern = "some{{nested:{stuff}}}";
+        let segments = parse_segments.parse(parse_pattern).unwrap();
+        assert_eq!(
+            segments,
+            vec![
+                Segment::Tag("some"),
+                Segment::Escaped('{'),
+                Segment::Tag("nested:"),
+                Segment::Value("stuff"),
+                Segment::Escaped('}')
+            ]
+        );
+    }
+
+    #[test]
+    fn test_ignores_rest() {
+        let parse_pattern = "millis: {millis},pos:{pos},current:{current}";
+        let segments = parse_segments.parse(parse_pattern).unwrap();
+        let mut parser = make_parser(segments);
+        let final_out = parser
+            .parse(b"a bunch of stuff{}{}{{}}.028millis: 1234.5,pos:-4.0,current:100,and a bunch of other stuff{}{}{{}}.028")
             .unwrap();
         assert_eq!(final_out, vec![1234.5, -4.0, 100.0]);
     }
