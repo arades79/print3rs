c9bf397af2f3633b528d05743ec426e4830f2c10
Skyelar Craver <skyelar.craver@cellink.com>
2024-03-08 12:59:04 -0500

    fix POC menu

--- print3rs-gui/src/components/app_menu.rs
+++ print3rs-gui/src/components/app_menu.rs
@@ -2,18 +2,33 @@ use iced::widget::text;
 
 use iced_aw::{menu, menu::Item, menu_bar};
 
-use crate::app::{App, AppElement};
+use crate::app::App;
+use crate::messages::Message;
 
-pub(crate) fn app_menu(_app: &App) -> AppElement<'_> {
-    let mb = menu_bar!((
-        text("File"),
-        menu!((text("Print"))(text("Clear"))(text("Save"))(text("Quit")))
-    )(
-        text("Settings"),
-        menu!((text("Log Level"))(text("Jog Increments"))(text(
-            "Probably something else"
-        )))
-    ))
+macro_rules! menu_template {
+     ($($x:tt)+) => {
+         menu!($($x)+).max_width(180.0).offset(6.0).spacing(5.0)
+     };
+ }
+
+pub(crate) fn app_menu(
+    _app: &App,
+) -> menu::MenuBar<'_, Message, <App as iced::Application>::Theme, iced::Renderer> {
+    #[rustfmt::skip]
+    let mb = 
+    menu_bar!(
+        (text("File"), menu_template!(
+            (text("Print"))
+            (text("Clear"))
+            (text("Save"))
+            (text("Quit"))
+        ))
+        (text("Settings"), menu_template!(
+            (text("Log Level"))
+            (text("Jog Increments"))
+            (text("Probably something else"))
+        ))
+    )
     .spacing(4.0);
-    mb.into()
+    mb
 }
