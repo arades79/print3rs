fcc61b8d013aa4df5b66d54f81fafb9d836707c3
Skyelar Craver <skyelar.craver@cellink.com>
2024-04-29 13:58:13 -0400

    refactor, test every branch

--- print3rs-commands/src/commands/help.rs
+++ print3rs-commands/src/commands/help.rs
@@ -25,17 +25,25 @@ disconnect                    disconnect from printer
 quit                          exit program
 \n";
 
+static PRINT_HELP: &str = "print: execute every line of G-code sequentially from the given file. The print job is added as a task which runs in the background with the filename as the task name. Other commands can be sent while a print is running, and a print can be stopped at any time with `stop`\n";
+static LOG_HELP: &str = "log: begin logging the specified pattern from the printer into a csv with the `name` given. This operation runs in the background and is added as a task which can be stopped with `stop`. The pattern given will be used to parse the logs, with values wrapped in `{}` being given a column of whatever is between the `{}`, and pulling a number in its place. If your pattern needs to include a literal `{` or `}`, double them up like `{{` or `}}` to have the parser read it as just a `{` or `}` in the output.\n";
+static REPEAT_HELP: &str = "repeat: repeat the given Gcodes (separated by gcode comment character `;`) in a loop until stopped. \n";
+static STOP_HELP: &str = "stop: stops a task running in the background. All background tasks are required to have a name, thus this command can be used to stop them. Tasks can also stop themselves if they fail or can complete, after which running this will do nothing.\n";
+static CONNECT_HELP: &str = "connect: Manually connect to a printer by specifying a protocol and some arguments. Arguments depend on protocol. For serial connection specify its path and optionally its baudrate. On windows this looks like `connect serial COM3 115200`, on linux more like `connect serial /dev/tty/ACM0 250000`. This does not test if the printer is capable of responding to messages, it will only open the port. Specifying no arguments will attempt autoconnection using serial.\n";
+static DISCONNECT_HELP: &str = "disconnect: disconnect from the currently connected printer. All active tasks will be stopped\n";
+static MACRO_HELP: &str = "create a case-insensitve alias to some set of gcodes, even containing other macros recursively to build up complex sets of builds with a single word. Macro names cannot be a single uppercase letter followed by a number, e.g. H105, to avoid conflict with Gcodes. Names can have any mix of alphanumeric, -, ., and _ characters. Commands in a macro are separated by ';', and macros can be used anywhere Gcodes are passed, including repeat commands and sends.\n";
+
 pub fn help(command: &str) -> &'static str {
     let command = command.trim();
 
     match command {
-        "print" => "print: execute every line of G-code sequentially from the given file. The print job is added as a task which runs in the background with the filename as the task name. Other commands can be sent while a print is running, and a print can be stopped at any time with `stop`\n",
-        "log" => "log: begin logging the specified pattern from the printer into a csv with the `name` given. This operation runs in the background and is added as a task which can be stopped with `stop`. The pattern given will be used to parse the logs, with values wrapped in `{}` being given a column of whatever is between the `{}`, and pulling a number in its place. If your pattern needs to include a literal `{` or `}`, double them up like `{{` or `}}` to have the parser read it as just a `{` or `}` in the output.\n",
-        "repeat" => "repeat: repeat the given Gcodes (separated by gcode comment character `;`) in a loop until stopped. \n",
-        "stop" => "stop: stops a task running in the background. All background tasks are required to have a name, thus this command can be used to stop them. Tasks can also stop themselves if they fail or can complete, after which running this will do nothing.\n",
-        "connect" => "connect: Manually connect to a printer by specifying a protocol and some arguments. Arguments depend on protocol. For serial connection specify its path and optionally its baudrate. On windows this looks like `connect serial COM3 115200`, on linux more like `connect serial /dev/tty/ACM0 250000`. This does not test if the printer is capable of responding to messages, it will only open the port. Specifying no arguments will attempt autoconnection using serial.\n",
-        "disconnect" => "disconnect: disconnect from the currently connected printer. All active tasks will be stopped\n",
-        "macro" => "create a case-insensitve alias to some set of gcodes, even containing other macros recursively to build up complex sets of builds with a single word. Macro names cannot start with G,T,M,N, or D to avoid conflict with Gcodes, and cannot have any non-alphanumeric characters. commands in a macro are separated by ';', and macros can be used anywhere Gcodes are passed, including repeat commands and sends.\n",
+        "print" => PRINT_HELP,
+        "log" => LOG_HELP,
+        "repeat" => REPEAT_HELP,
+        "stop" => STOP_HELP,
+        "connect" => CONNECT_HELP,
+        "disconnect" => DISCONNECT_HELP,
+        "macro" => MACRO_HELP,
         _ => FULL_HELP,
     }
 }
@@ -44,6 +52,11 @@ pub fn help(command: &str) -> &'static str {
 #[test]
 fn test_help() {
     assert_eq!(help(""), FULL_HELP);
-    assert_ne!(help("print "), help(" stop"));
-    assert_ne!(help(" connect "), help("disconnect"));
+    assert_eq!(help("print"), PRINT_HELP);
+    assert_eq!(help("log"), LOG_HELP);
+    assert_eq!(help("repeat"), REPEAT_HELP);
+    assert_eq!(help("stop"), STOP_HELP);
+    assert_eq!(help("connect"), CONNECT_HELP);
+    assert_eq!(help("disconnect"), DISCONNECT_HELP);
+    assert_eq!(help("macro"), MACRO_HELP);
 }
